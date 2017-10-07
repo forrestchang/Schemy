f06890722346b18e8ca8e61caa1e184a57f34fcc
Forrest <forrestchang7@gmail.com>
2017-10-07 14:11:01 +0800

    feat: finish parser

---
+++ schemy/schemy.py
@@ -0,0 +1,37 @@
+Symbol = str
+List = list
+Number = (int, float)
+
+
+def tokenize(chars):
+    return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+
+
+def parse(program):
+    return read_from_tokens(tokenize(program))
+
+
+def read_from_tokens(tokens):
+    if len(tokens) == 0:
+        raise SyntaxError('unexpected EOF while reading')
+    token = tokens.pop(0)
+    if token == '(':
+        L = []
+        while tokens[0] != ')':
+            L.append(read_from_tokens(tokens))
+        tokens.pop(0)   # pop off ')'
+        return L
+    elif token == ')':
+        raise SyntaxError('unexpected )')
+    else:
+        return atom(token)
+
+
+def atom(token):
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return fload(token)
+        except ValueError:
+            return Symbol(token)

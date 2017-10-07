78984dbfd0689b800c0774cb26ba55d9999c7042
Forrest <forrestchang7@gmail.com>
2017-10-07 18:17:52 +0800

    feat: add repl

--- schemy/schemy.py
+++ schemy/schemy.py
@@ -92,3 +92,32 @@ def eval(x, env=global_env):
         proc = eval(x[0], env)
         args = [eval(arg, env) for arg in x[1:]]
         return proc(*args)
+
+
+# repl: read-eval-print-loop
+def repl(prompt='schemy>>> '):
+    while True:
+        try:
+            val = eval(parse(raw_input(prompt)))
+            if val is not None:
+                print(schemestr(val))
+        except (SyntaxError, TypeError) as e:
+            print(e)
+        except KeyError as e:
+            print('invalid key', e.args)
+
+
+
+def schemestr(exp):
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')'
+    else:
+        return str(exp)
+
+
+def main():
+    repl()
+
+
+if __name__ == '__main__':
+    main()

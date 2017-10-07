897450fb4de3115623c950aeef2f86f333f284b1
Forrest <forrestchang7@gmail.com>
2017-10-07 18:03:10 +0800

    test: add test for eval

--- tests/test_schemy.py
+++ tests/test_schemy.py
@@ -1,8 +1,8 @@
 import unittest
-from schemy.schemy import tokenize, parse
+from schemy.schemy import tokenize, parse, eval
 
 
-class TestParser(unittest.TestCase):
+class TestSchemy(unittest.TestCase):
 
     def test_tokenize(self):
         self.assertEqual(tokenize('(+ 1 2)'), ['(', '+', '1', '2', ')'])
@@ -11,6 +11,10 @@ class TestParser(unittest.TestCase):
         program = "(begin (define r 10) (* pi (* r r)))"
         result = ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]
         self.assertEqual(parse(program), result)
+
+    def test_eval(self):
+        program = "(begin (define r 10) (* 3.14 (* r r)))"
+        self.assertEqual(eval(parse(program)), 3.14*10*10)
 
 
 if __name__ == '__main__':

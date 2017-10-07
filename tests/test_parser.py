888cc653a501d5e7a4d3accb177375373d736be5
Forrest <forrestchang7@gmail.com>
2017-10-07 17:38:28 +0800

    test: add test for parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,11 +1,16 @@
 import unittest
-from schemy.schemy import tokenize
+from schemy.schemy import tokenize, parse
 
 
 class TestParser(unittest.TestCase):
 
     def test_tokenize(self):
         self.assertEqual(tokenize('(+ 1 2)'), ['(', '+', '1', '2', ')'])
+
+    def test_parser(self):
+        program = "(begin (define r 10) (* pi (* r r)))"
+        result = ['begin', ['define', 'r', 10], ['*', 'pi', ['*', 'r', 'r']]]
+        self.assertEqual(parse(program), result)
 
 
 if __name__ == '__main__':

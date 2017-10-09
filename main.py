942620f8ef81da5546ae8e2d5533dbb15b6bf4fc
Forrest <forrestchang7@gmail.com>
2017-10-09 21:09:38 +0800

    refactor: add main file

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Author: Forrest Chang (forrestchang7@gmail.com)
+
+from schemy.repl import repl
+
+
+def main():
+    repl()
+
+
+if __name__ == '__main__':
+    main()
--- schemy/schemy.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Author: Forrest Chang (forrestchang7@gmail.com)
-
-from .repl import repl
-
-repl()

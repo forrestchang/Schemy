750c0b6fec955d962ebd46db916b899b2ada4911
Forrest <forrestchang7@gmail.com>
2017-10-18 17:17:19 +0800

    feat: add boolean types

--- schemy/types.py
+++ schemy/types.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Author: Forrest Chang (forrestchang7@gmail.com)
-from schemy.exception import bad_type
+import numbers
+
+from schemy.exception import bad_type, SchemeError
 
 
 class SchemeValue:
@@ -118,6 +120,102 @@ class SchemeValue:
 
     def cdr(self):
         bad_type(self, 0, "cdr")
+
+    def stringp(self):
+        return scheme_false
+
+    def symbolp(self):
+        return scheme_false
+
+    def numberp(self):
+        return scheme_false
+
+    def integerp(self):
+        return scheme_false
+
+    def evaluate_arguments(self, arg_list, env):
+        """
+        Evaluate the expression in ARG_LIST in env to produce arguments for this procedure.
+
+        Returns an iterable of the results.
+        """
+        msg = 'attempt to call something of non-function type ({})'
+        raise SchemeError(msg.format(type(self).__name__))
+
+    def apply(self, args, env):
+        """
+        Apply self to a Scheme list of args in environment ENV. Returns either
+            A. A tuple (val, None), where val is the value this procedure
+                returns when given args and called in env.
+            B. A tuple (expr1, env1), where env1 will yield the same result
+                and the same effects as evaluating args in env.
+        """
+        msg = "attempt to call something of non-function type ({0})"
+        raise SchemeError(msg.format(type(self).__name__))
+
+    def get_actual_value(self):
+        """
+        Returns any desired transformation of a value newly fetched from a
+        symbol before it is used.
+        """
+        return self
+
+
+def scheme_coerce(x):
+    """
+    Returns the Scheme value corresponding to X. Converts numbers to
+    SchemeNumbers and strings to interned symbols.
+    SchemeValue are unchanged, other values raise a TypeError.
+    """
+    if isinstance(x, SchemeValue):
+        return x
+    elif isinstance(x, numbers.Number):
+        return scnum(x)
+    elif isinstance(x, str):
+        return intern(x)
+    else:
+        raise TypeError('cannot covert type {} to a SchemeValue').format(type(x))
+
+
+class okay(SchemeValue):
+    """Signifies an undefined value."""
+    def __repr__(self):
+        return 'okay'
+
+okay = okay() # there is only one instance
+
+# -------
+# Booleans
+# -------
+
+class scheme_true(SchemeValue):
+
+    def booleanp(self):
+        return scheme_true
+
+    def __repr__(self):
+        return 'scheme_true'
+
+    def __str__(self):
+        return '#t'
+
+
+class scheme_false(SchemeValue):
+
+    def __bool__(self):
+        return False
+
+    def booleanp(self):
+        return scheme_true
+
+    def __repr__(self):
+        return 'scheme_false'
+
+    def __str__(self):
+        return '#f'
+
+scheme_true = scheme_true()
+scheme_false = scheme_false()
 
 
 def scbool(x):

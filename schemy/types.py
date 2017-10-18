3a37ecdfefa078334ba93e9e43431d1e699212b0
Forrest <forrestchang7@gmail.com>
2017-10-18 16:49:02 +0800

    feat: update types

--- schemy/types.py
+++ schemy/types.py
@@ -1,7 +1,124 @@
 # -*- coding: utf-8 -*-
 # Author: Forrest Chang (forrestchang7@gmail.com)
+from schemy.exception import bad_type
 
 
-Symbol = str
-List = list
-Number = (int, float)
+class SchemeValue:
+    """
+    The parent class of all Scheme values manipulated by the interpreter.
+    The methods here give default implementations, and are overridden in the
+    subclasses of SchemeValue.
+    """
+
+    def __bool__(self):
+        """
+        This is the method used by Python's conditionals (if, and, or, not, while)
+        to determine whether some value is to be treated as meaning 'true'.
+
+        True if I am supposed to count as a 'True value' in Python.
+        """
+        return True
+
+    def print_repr(self):
+        """The string printed for me by the Scheme 'print' procedure."""
+        return str(self)
+
+    # The following methods return SchemeValue.
+    #
+    # Those ending in 'p'(predicate) return scheme_false to mean false, and some other SchemeValue
+    # (often scheme_true) to mean 'true'.
+
+    def booleanp(self):
+        return scheme_false
+
+    def notp(self):
+        return scbool(not self)
+
+    def eqp(self, y):
+        return scbool(self is y)
+
+    def eqvp(self, y):
+        """
+        True if self equivalent to y (同一个对象).
+        """
+        return scbool(self is y)
+
+    def equalp(self, y):
+        """
+        True if self equal in value to y (值相同).
+        """
+        return scbool(self == y)
+
+    def tomp(self):
+        return scheme_true
+
+    def pairp(self):
+        return scheme_false
+
+    def nullp(self):
+        return scheme_false
+
+    def listp(self):
+        return scheme_false
+
+    def length(self):
+        bad_type(self, 0, 'length')
+
+    def neg(self):
+        """Unary negation (as in -x)"""
+        bad_type(self, 0, 'sub')
+
+    def quo(self, y):
+        bad_type(self, 0, 'quotient')
+
+    def modulo(self, y):
+        bad_type(self, 0, 'modulo')
+
+    def rem(self, y):
+        bad_type(self, 0, 'remainder')
+
+    def floor(self):
+        bad_type(self, 0, 'floor')
+
+    def ceil(self):
+        bad_type(self, 0, 'ceil')
+
+    def eq(self, y):
+        bad_type(self, 0, '=')
+
+    def ltp(self, y):
+        bad_type(self, 0, '<')
+
+    def gtp(self, y):
+        bad_type(self, 0, '>')
+
+    def lep(self, y):
+        bad_type(self, 0, "<=")
+
+    def gep(self, y):
+        bad_type(self, 0, ">=")
+
+    def evenp(self):
+        bad_type(self, 0, "even?")
+
+    def oddp(self):
+        bad_type(self, 0, "odd?")
+
+    def zerop(self):
+        bad_type(self, 0, "zero?")
+
+    def cons(self, y):
+        return Pair(self, y)
+
+    def append(self, y):
+        bad_type(self, 0, "append")
+
+    def car(self):
+        bad_type(self, 0, "car")
+
+    def cdr(self):
+        bad_type(self, 0, "cdr")
+
+
+def scbool(x):
+    pass

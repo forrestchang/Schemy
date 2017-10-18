a9d1c653db70ab9524544070f8f185f713bb0bae
Forrest <forrestchang7@gmail.com>
2017-10-18 16:13:40 +0800

    refactor: rewirte tokenizer

--- schemy/tokenizer.py
+++ schemy/tokenizer.py
@@ -1,38 +1,64 @@
 # -*- coding: utf-8 -*-
 # Author: Forrest Chang (forrestchang7@gmail.com)
+from .utils import main
+import itertools
+import string
+import sys
+import tokenize
 
-from .types import Symbol
-
-
-def tokenize(chars):
-    return chars.replace('(', ' ( ').replace(')', ' ) ').split()
-
-
-def read_from_tokens(tokens):
-    if len(tokens) == 0:
-        raise SyntaxError('unexpected EOF while reading')
-    token = tokens.pop(0)
-    if token == '(':
-        L = []
-        while tokens[0] != ')':
-            L.append(read_from_tokens(tokens))
-        tokens.pop(0)   # pop off ')'
-        return L
-    elif token == ')':
-        raise SyntaxError('unexpected )')
-    else:
-        return atom(token)
+_NUMERAL_STARTS = set(string.digits) | set('+-.')
+_SYMBOL_CHARS = (set('!$%&*/:<=>?@^_~') | set(string.ascii_lowercase) | set(string.ascii_uppercase) | _NUMERAL_STARTS)
+_STRING_DELIMS = set('"')
+_WHITESPACE = set(' \t\n\r')
+_SINGLE_CHAR_TOKENS = set("()'`")
+_TOKEN_END = _WHITESPACE | _SINGLE_CHAR_TOKENS | _STRING_DELIMS | {',', ',@'}
+DELIMITERS = _SINGLE_CHAR_TOKENS | {'.', ',', ',@'}
 
 
-def atom(token):
-    try:
-        return int(token)
-    except ValueError:
-        try:
-            return float(token)
-        except ValueError:
-            return Symbol(token)
+def valid_symbol(s):
+    """ Return whethre s is not a well-formed value. """
+    if len(s) == 0:
+        return False
+    for c in s:
+        if c not in _SYMBOL_CHARS:
+            return False
+    return True
 
 
-def parse(program):
-    return read_from_tokens(tokenize(program))
+def next_candidate_token(line, k):
+    """
+    A tuple (tok, k), where tok is the next substring of line at or after position k that could be a token,
+    and k is the position in line following that token.
+
+    Returns (None, len(line)) when there are no more tokens.
+    """
+    while k < len(line):
+        c = line[k]
+        if c == ';':
+            return None, len(line)
+        elif c in _WHITESPACE:
+            k += 1
+        elif c in _SINGLE_CHAR_TOKENS:
+            return c, k+1
+        elif c == '#': # Boolean #t or #f
+            return line[k:k+2], min(k+2, len(line))
+        elif c == ',': # Unquote; check for @
+            if k+1 < len(line) and line[k+1] == '@':
+                return ',@', k+2
+            return c, k+1
+        elif c in _STRING_DELIMS:
+            if k+1 < len(line) and line[k+1] == c: # No triple quotes in Scheme
+                return c+c, k+2
+            line_bytes = (bytes(line[k:], encoding='utf-8'),)
+            gen = tokenize.tokenize(iter(line_bytes).__next__)
+            next(gen) # Throw away encoding token
+            token = next(gen)
+            if token.type != tokenize.STRING:
+                raise ValueError('invalid string: {}'.format(token.string))
+            return token.string, token.end[1]+k
+        else:
+            j = k
+            while j < len(line) and line[j] not in _TOKEN_END:
+                j += 1
+            return line[k:j], min(j, len(line))
+    return None, len(line)

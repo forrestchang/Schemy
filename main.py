90b3321923d1a1adc2d50bdac783e96316b8ad68
Forrest <forrestchang7@gmail.com>
2017-10-19 20:08:48 +0800

    refactor: rewrite main function

--- main.py
+++ main.py
@@ -2,11 +2,8 @@
 # Author: Forrest Chang (forrestchang7@gmail.com)
 
 from schemy.eval import run
+from schemy.utils import main
 
-
-def main():
+@main
+def repl():
     run()
-
-
-if __name__ == '__main__':
-    main()
